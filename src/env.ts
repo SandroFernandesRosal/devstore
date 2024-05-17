d2be36219abf065d8491a4493ee51e8a6af580aa
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2024-05-17 01:35:09 -0300

    .

--- src/env.ts
+++ src/env.ts
@@ -7,13 +7,4 @@ const envSchema = z.object({
 
 const parsedEnv = envSchema.safeParse(process.env)
 
-if (!parsedEnv.success) {
-  console.error(
-    'Invalid environment variables',
-    parsedEnv.error.flatten().fieldErrors,
-  )
-
-  throw new Error('Invalid environment variables.')
-}
-
 export const env = parsedEnv.data
