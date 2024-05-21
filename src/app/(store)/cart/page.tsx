9c960adba412e3d411b6d1da4c170e1d2161e63d
Sandro Fernandes <sandrofernandesrosal@gmail.com>
2024-05-21 01:23:37 -0300

    refatorando

--- src/app/(store)/cart/page.tsx
+++ src/app/(store)/cart/page.tsx
@@ -13,21 +13,29 @@ export default function Cart() {
       <div className="flex flex-wrap gap-4 justify-center w-full">
         {items.map((product) => {
           return (
-            <Link
+            <div
               key={product.productId}
-              href={`/product/${product.slug}`}
               className="group items-center flex-col rounded-lg bg-zinc-900 flex justify-center max-w-[75%] md:max-w-[35%] lg:max-w-[25%] pb-4 "
             >
-              <Image
-                src={product.image}
-                className="group-hover:scale-105 transition-transform duration-500"
-                width={480}
-                height={480}
-                alt=""
-                quality={100}
-              />
+              <Link href={`/product/${product.slug}`}>
+                <Image
+                  src={product.image}
+                  className="group-hover:scale-105 transition-transform duration-500"
+                  width={480}
+                  height={480}
+                  alt=""
+                  quality={100}
+                />{' '}
+              </Link>
+
               <div className="md:h-12 h-8 z-20 flex my-2 items-center gap-2 max-w-[280px] w-[90%] justify-between rounded-full border-2 border-zinc-500 bg-black/60 p-1 ">
-                <span className="text-sm truncate pl-1">{product.title}</span>
+                <Link href={`/product/${product.slug}`}>
+                  {' '}
+                  <span className="text-sm truncate pl-1">
+                    {product.title}
+                  </span>{' '}
+                </Link>
+
                 <span className="flex h-full items-center justify-center rounded-full bg-green-700 px-4 font-semibold">
                   {product.price.toLocaleString('pt-BR', {
                     style: 'currency',
@@ -42,7 +50,7 @@ export default function Cart() {
                 productId={product.productId}
                 quantity={product.quantity}
               />
-            </Link>
+            </div>
           )
         })}
       </div>
